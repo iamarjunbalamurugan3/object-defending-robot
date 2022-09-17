13fca7c5df9ac4c7fd44c2f7f6d810e560a99fd2
iamarjunbalamurugan3 <113736647+iamarjunbalamurugan3@users.noreply.github.com>
2022-09-17 11:46:56 +1000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ def on_forever():
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 50)
     elif sensor < 10:
         Kitronik_Move_Motor.stop()
-        basic.pause (500)
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.REVERSE, 50)
-        basic.pause(1000)
-        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT,50)
+        basic.pause (250)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.REVERSE, 25)
+        basic.pause(500)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT,10)
+        basic.pause(250)
+        Kitronik_Move_Motor.stop()
+
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 let sensor = 0
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     sensor = Kitronik_Move_Motor.measure()
     if (sensor > 10) {
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
     } else if (sensor < 10) {
         Kitronik_Move_Motor.stop()
+        basic.pause(250)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 25)
         basic.pause(500)
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
-        basic.pause(1000)
-        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 50)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 10)
+        basic.pause(250)
+        Kitronik_Move_Motor.stop()
     }
-    
 })
