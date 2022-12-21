9386cfc8a4ff147bc7d079d2a681f030205b2ef2
iamarjunbalamurugan3 <113736647+iamarjunbalamurugan3@users.noreply.github.com>
2022-12-22 10:10:04 +1100

    better

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def on_forever():
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 50)
     elif sensor < 10:
         Kitronik_Move_Motor.stop()
-        basic.pause (250)
+        basic.pause(250)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.REVERSE, 25)
         basic.pause(500)
-        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT,10)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.RIGHT, 25)
         basic.pause(250)
         Kitronik_Move_Motor.stop()
-
-
 basic.forever(on_forever)
+
+def on_forever2():
+    Kitronik_Move_Motor.create_move_motor_zipled(4)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
 let sensor = 0
+let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+let rear = moveMotorZIP.range(0, 2)
+let head = moveMotorZIP.range(0, 2)
+basic.forever(function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    head.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
+    rear.setZipLedColor(2, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green))
+})
 basic.forever(function () {
     sensor = Kitronik_Move_Motor.measure()
     if (sensor > 10) {
@@ -8,7 +16,7 @@ basic.forever(function () {
         basic.pause(250)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 25)
         basic.pause(500)
-        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 10)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 25)
         basic.pause(250)
         Kitronik_Move_Motor.stop()
     }
